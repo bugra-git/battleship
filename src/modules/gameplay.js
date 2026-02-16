80c59f77e990e8a952c5685b9a0f1983356eacce
bugra-git <237065860+bugra-git@users.noreply.github.com>
2026-02-16 22:34:58 +0300

    Create module for gameplay

---
+++ src/modules/gameplay.js
@@ -0,0 +1,24 @@
+import { Ship, Gameboard, Player } from "./gameLogic.js";
+
+class Game {
+  constructor() {
+    this.human = new Player();
+    this.comp = new Player("computer");
+    this.shipSizes = [5, 4, 3, 3, 2];
+    this.humanShips = this.shipSizes.map((length) => {
+      new Ship(length);
+    });
+    this.humanShips[0].coords = [0,0];
+    this.humanShips[0].alignment = "vertical";
+    this.humanShips[1].coords = [7,6];
+    this.humanShips[1].alignment = "horizontal";
+    this.humanShips[2].coords = [0,5];
+    this.humanShips[2].alignment = "horizontal";
+    this.humanShips[3].coords = [4,4];
+    this.humanShips[3].alignment = "vertical";
+    this.humanShips[4].coords = [8,2];
+    this.humanShips[4].alignment = "horizontal";
+  }
+}
+
+export { Game };
