f502979c75688d922f633b48ecd3001973be6087
bugra-git <237065860+bugra-git@users.noreply.github.com>
2026-02-16 23:56:22 +0300

    Implement initiating human ships

--- src/modules/gameplay.js
+++ src/modules/gameplay.js
@@ -5,19 +5,25 @@ class Game {
     this.human = new Player();
     this.comp = new Player("computer");
     this.shipSizes = [5, 4, 3, 3, 2];
-    this.humanShips = this.shipSizes.map((length) => {
-      new Ship(length);
-    });
-    this.humanShips[0].coords = [0,0];
+    this.humanShips = this.shipSizes.map((length) => ({
+      ship: new Ship(length),
+    }));
+    this.humanShips[0].coords = [0, 0];
     this.humanShips[0].alignment = "vertical";
-    this.humanShips[1].coords = [7,6];
+    this.humanShips[1].coords = [7, 6];
     this.humanShips[1].alignment = "horizontal";
-    this.humanShips[2].coords = [0,5];
+    this.humanShips[2].coords = [0, 5];
     this.humanShips[2].alignment = "horizontal";
-    this.humanShips[3].coords = [4,4];
+    this.humanShips[3].coords = [4, 4];
     this.humanShips[3].alignment = "vertical";
-    this.humanShips[4].coords = [8,2];
+    this.humanShips[4].coords = [8, 2];
     this.humanShips[4].alignment = "horizontal";
+  }
+
+  initHumanShips() {
+    this.humanShips.forEach((s) => {
+      this.human.board.placeShip(s.ship, s.coords, s.alignment);
+    });
   }
 }
 
---
+++ tests/gameplay.test.js
@@ -0,0 +1,12 @@
+import { Game } from "../src/modules/gameplay.js";
+
+test("human ships can be placed on board", () => {
+  const game = new Game();
+  game.initHumanShips();
+  expect(game.human.board.ships.length).toBe(5);
+  expect(game.human.board.grid[0][0].ship).toBe(game.human.board.ships[0]);
+  expect(game.human.board.grid[7][6].ship).toBe(game.human.board.ships[1]);
+  expect(game.human.board.grid[0][5].ship).toBe(game.human.board.ships[2]);
+  expect(game.human.board.grid[4][4].ship).toBe(game.human.board.ships[3]);
+  expect(game.human.board.grid[8][2].ship).toBe(game.human.board.ships[4]);
+});
